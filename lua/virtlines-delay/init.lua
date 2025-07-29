511d05acbb1077fefd8bfd933de4c09484b8cce2
CoreyB <coreyb@noreply.com>
2025-07-29 21:27:43 +1000

    Respects previous virtual_lines settings, including format

--- lua/virtlines-delay/init.lua
+++ lua/virtlines-delay/init.lua
@@ -1,37 +1,65 @@
 local M = {}
 
-local const_plugin = "virtlines-delay"
-local config = require(const_plugin .. ".config")
+local const_plugin_name = "virtlines-delay"
+local config = require(const_plugin_name .. ".config")
+
 local const_augroup = "vl_delay"
+
 local t = vim.uv.new_timer()
-local lastline = -1
+
+local last_line = -1
+local original_config
+local disabled = false
+
+local function disable_vl()
+	if disabled == false then
+		disabled = true
+		-- save format and original setting for later
+		original_config = vim.diagnostic.config()
+		vim.diagnostic.config({ virtual_lines = false })
+	end
+end
+
+local function enable_vl()
+	if disabled then
+		disabled = false
+		-- merge original virtual_lines settings with current config
+		local new_conf = vim.tbl_deep_extend("keep", original_config, { virtual_lines = { current_line = true } })
+		vim.diagnostic.config({ virtual_lines = new_conf.virtual_lines })
+	end
+end
 
 local function start_timer()
-	if t ~= nil then
-		t:stop()
+	if (t ~= nil) and (t:is_active() == false) then
 		t:start(
 			config.delay,
 			0,
 			vim.schedule_wrap(function()
-				vim.diagnostic.config({ virtual_lines = { current_line = true } })
+				enable_vl()
 			end)
 		)
 	end
 end
 
+local function check_moved()
+	local currentline = vim.fn.getcurpos(0)[2]
+	if currentline ~= last_line then
+		last_line = currentline
+		disable_vl()
+		if t ~= nil then
+			t:stop()
+		end
+	else
+		start_timer()
+	end
+end
+
 function M.setup(opts)
 	config = vim.tbl_deep_extend("keep", opts, config)
-	local group = vim.api.nvim_create_augroup(const_augroup, { clear = true })
-	vim.api.nvim_create_autocmd("CursorMoved", {
-		group = const_augroup,
-		callback = function()
-			local currentline = vim.fn.getcurpos(0)[2]
-			if currentline ~= lastline then
-				lastline = currentline
-				start_timer()
-				vim.diagnostic.config({ virtual_lines = false })
-			end
-		end,
+	local augroup = vim.api.nvim_create_augroup(const_augroup, { clear = true })
+	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorHold" }, {
+		group = augroup,
+		callback = check_moved,
 	})
 end
 
